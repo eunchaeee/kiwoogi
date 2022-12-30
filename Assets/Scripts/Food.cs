ffb8e267d9cd0163d39fac7bfa68287f4a9bf87a
eunchaeee <ddolchae513@gmail.com>
2022-12-30 17:30:52 +0900

    1. make food control logic 2. change plane to quad

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -4,5 +4,27 @@ using UnityEngine;
 
 public class Food : MonoBehaviour
 {
-    // TODO: Make Food always follow mouse position.
+    private Vector3 pos;
+    public float offset = 3f;
+    private bool followCursor = true;
+
+    private void Update()
+    {
+        if (followCursor)
+        {
+            pos = Input.mousePosition;
+            pos.z = offset;
+            transform.position = Camera.main.ScreenToWorldPoint(pos);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            followCursor = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Food : OnCollisionEnter()");
+    }
 }
